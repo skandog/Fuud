d6eed83181a668d3e4492449388aa7a25abad1c3
skandog <muumrar@gmail.com>
2023-02-05 16:49:07 +0000

    started building out the basic UI, setting up safearea view for ios and android

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Platform,
+} from "react-native";
 
+const isAndroid = Platform.OS === "android";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Woof up App.tsx to start working on your app!</Text>
-      {/* <StatusBar style="auto" /> */}
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: isAndroid ? StatusBar.currentHeight : 0,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 16,
+  },
+  search: {
+    backgroundColor: "green",
+    padding: 16,
   },
 });
